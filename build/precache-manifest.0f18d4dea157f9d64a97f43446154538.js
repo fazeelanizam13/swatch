f26de72b073ed366fe32737527fb4dda81d6db28
Fazeela Nizam <42344151+fazeelanizam13@users.noreply.github.com>
2020-08-24 09:20:23 +0530

    Add files via upload

---
+++ build/precache-manifest.0f18d4dea157f9d64a97f43446154538.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75776868ceda84d0198e905181f34b1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13d8a6d931ee43740693",
+    "url": "./static/css/main.e5bf0cef.chunk.css"
+  },
+  {
+    "revision": "0c06ae4a556248003be8",
+    "url": "./static/js/2.59f82525.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "./static/js/2.59f82525.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13d8a6d931ee43740693",
+    "url": "./static/js/main.841411d8.chunk.js"
+  },
+  {
+    "revision": "2f981ce922b5ac0ee3a1",
+    "url": "./static/js/runtime-main.61141fb2.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "./static/media/Raleway-Regular.9942588a.ttf"
+  }
+]);
